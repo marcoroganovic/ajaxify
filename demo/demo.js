8611c741213c22cb7c70e489506ca122bbcb721c
Marco Roganovic <marcoroganovic@gmail.com>
2017-01-06 21:32:10 +0100

    add demo of library usage

---
+++ demo/demo.js
@@ -0,0 +1,38 @@
+(function() {
+  
+  var root = "http://jsonplaceholder.typicode.com";
+  var Collection;
+
+  // cache DOM elements
+  var button = document.querySelector(".get-posts");
+  var posts = document.querySelector(".posts");
+  
+  function renderPosts(collection) {
+    posts.innerHTML = "";
+    collection.forEach(article => {
+      posts.innerHTML += renderArticleUI(article);
+    });
+  }
+
+  function renderArticleUI(data) {
+    return `<article><h1>${data.title}</h1><p>${data.body}</p></article>`;
+  }
+
+  var getPosts = Ajax.get({
+    url: root + "/posts",
+
+    success: function(data) {
+      Collection = JSON.parse(data);
+      renderPosts(Collection);
+    },
+
+    failure: function() {
+      posts.innerHTML = "Loading...";
+    }
+  });
+
+  button.addEventListener("click", function(e) {
+    getPosts.send();
+  });
+  
+})();
